ae8cb0938590c033ce46c73bada4dc298e91a50c
ripal <ripal@promactinfo.com>
2016-01-06 19:24:44 +0530

    the new work

---
+++ final/Employee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final
+{
+    public class Employee : IEmployee
+    {
+        public int flag;
+        public string Id, Name, City, Email = "only for HR", Skype = "only for developer", id1;
+        public Employee()
+        {
+
+        }
+        public Employee(string id, string name, string city)
+        {
+            Id = id;
+            Name = name;
+            City = city;
+        }
+
+
+        public virtual void Display()
+        {
+            Console.WriteLine("\n Details are : \n Id: {0} \n Name: {1} \n City: {2} ", Id, Name, City);
+        }
+        public void Display(List<Employee> list)
+        {
+            foreach (Employee employee1 in list)
+            {
+                employee1.Display();
+            }
+        }
+        public void Find(List<Employee> list)
+        {
+            Console.WriteLine("\n Enter the id of the employee to find");
+            id1 = Console.ReadLine();
+            foreach (Employee employee2 in list)
+            {
+                if (employee2.Id == id1)
+                {
+                    Console.WriteLine("the employee is:\n");
+                    employee2.Display();
+                    flag = 1;
+
+                }
+
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("not found");
+            }
+        }
+        // ~Employee()
+        // {
+        //     Console.WriteLine("\n Details are : \n Id: {0} \n Name: {1} \n City: {2} \n Email: {3}", Id, Name, City, Email);
+
+        // }
+    }
+
+    public class Hr : Employee
+    {
+        public Hr()
+        {
+
+        }
+        public Hr(string id, string name, string city, string email) : base(id,name,city)
+        {
+            Email = email;
+        }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("email: {0}",Email);
+        }
+       
+    }
+
+    public class Developer : Employee, IEmployee
+    {
+        public Developer()
+        {
+
+        }
+        public Developer(string id, string name, string city, string skype) :base(id,name,city)
+        {
+            
+            Skype = skype;
+        }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Skype address: {0}", Skype);
+        }
+    }
+}
---
+++ final/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final
+{
+    public interface IEmployee
+    {
+    
+        void Display(List<Employee> list);
+        void Find(List<Employee> list);
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            string id,name,city,email,skype;
+           
+            List<Employee> list = new List<Employee>();
+
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("\n 0.Exit \n 1.Register Employee\n 2.Register Hr \n 3.Register Developer \n 4.Find \n 5.Display Whole List");
+                Console.WriteLine("enter your choice\n");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                Employee employee = new Employee();
+
+
+                switch(choice)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        Console.WriteLine("\n Id:");
+                        id = Console.ReadLine();
+                        Console.WriteLine("\n Name:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("\n City :");
+                        city = Console.ReadLine();
+                        Console.WriteLine("\n Employee Registered");
+                        list.Add(new Employee(id,name,city));
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("\n Id:");
+                        id = Console.ReadLine();
+                        Console.WriteLine("\n Name:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("\n City :");
+                        city = Console.ReadLine();
+                        Console.WriteLine("\n Email :");
+                        email = Console.ReadLine();
+                        Console.WriteLine("\n Hr Registered");
+                        list.Add(new Hr(id,name,city,email));
+                        Console.ReadKey();
+                        break;
+                    
+                    case 3:
+
+                        Console.WriteLine("\n Id:");
+                        id = Console.ReadLine();
+                        Console.WriteLine("\n Name:");
+                        name = Console.ReadLine();
+                        Console.WriteLine("\n City :");
+                        city = Console.ReadLine();
+                        Console.WriteLine("\n Skype Address :");
+                        skype = Console.ReadLine();
+                        Console.WriteLine("\n Developer Registered");
+                        list.Add(new Developer(id,name,city,skype));
+                        Console.ReadKey();
+                        break;
+
+                    case 4:
+                        employee.Find(list);
+                        Console.ReadKey();
+                        break;
+                
+                    case 5:
+                        Console.WriteLine("\nThe number of employees in list are :");
+                        Console.WriteLine(list.Count);
+                        employee.Display(list);
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            
+        }
+    }
+        
+    }
+
